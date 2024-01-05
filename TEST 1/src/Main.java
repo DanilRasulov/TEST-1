207bc0159e9738247287999d5f19fd6e2130c909
Данил <drasulovdanil@gmail.com>
2024-01-05 19:41:07 +0200

    версия 4

--- TEST 1/src/Main.java
+++ TEST 1/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 12;
-        double b = 12.1;
+        int a = 13;
+        double b = 13.1;
         System.out.println(a + b);
     }
 }
